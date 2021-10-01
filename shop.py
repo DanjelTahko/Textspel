a42cbf3437b0eda243efec3e7b842577e156564a
Daniel Tahko <daniel.tahko@hotmail.com>
2021-10-01 20:29:21 +0200

    Updated SHOP with Items there, NEXT- remove items from main

--- shop.py
+++ shop.py
@@ -1,4 +1,8 @@
 
+from character import Character
+from item import Item
+
+
 def mainMenu():
     print("\n -------------------- ")
     print("|    *\Shop Menu/*   |")
@@ -18,7 +22,7 @@ def weaponMenu():
     print("| 2. Spear  |  Deals damage between 1 - 8  | Cost: 15 |")
     print("| 3. Axe    |  Deals damage between 1 - 10 | Cost: 20 |")
     print("| 4. Sword  |  Delas damage between 3 - 15 | Cost: 50 |")
-    print("| 5. Bomb   |  Deals damage between 6 - 7  | Cost: 50 |") 
+    print("| 5. Bombs  |  Deals damage between 6 - 7  | Cost: 50 |") 
     print("|-----------------------------------------------------|")
     print("| 6. Back                                             |") 
     print(" ----------------------------------------------------- ") 
@@ -35,25 +39,49 @@ def potionMenu():
     print("| 5. Back                                                          |") 
     print(" ------------------------------------------------------------------ ") 
 
-def inStore(coins):
+def getItemsFromInventory(inventory):
+    inventoryStrings = []
+    for i in inventory:
+        inventoryStrings.append(i.getItem())
+    return inventoryStrings
+
+def inStore(player: Character):
     while True:
+        coins = player.getCoins()
+        inventory = player.getInventory()
         mainMenu()
         menuChoice = input("Select menu option: ")
         if menuChoice.isdigit() and int(menuChoice) == 1:
             while True:
                 weaponMenu()
-                print(f"Your coins: {coins}")
+                print(f"Your coins: {player.getCoins()}")
                 weaponChoice = input("Select menu option: ")
-                if weaponChoice.isdigit() and int(weaponChoice) == 1 and coins >= 10:
+
+                if weaponChoice.isdigit() and int(weaponChoice) == 1 and coins >= 10 and "knife" not in getItemsFromInventory(inventory):
                     print("* Added knife to inventory") #(coins -= 10) IF not in inventory
-                elif weaponChoice.isdigit() and int(weaponChoice) == 2 and coins >= 15:
+                    player.addToInventory(knife)
+                    player.loseCoins(10)
+
+                elif weaponChoice.isdigit() and int(weaponChoice) == 2 and coins >= 15 and "spear" not in getItemsFromInventory(inventory):
                     print("* Added spear to inventory") # (coins -= 15)
-                elif weaponChoice.isdigit() and int(weaponChoice) == 3 and coins >= 20:
+                    player.addToInventory(spear)
+                    player.loseCoins(15)
+
+                elif weaponChoice.isdigit() and int(weaponChoice) == 3 and coins >= 20 and "axe" not in getItemsFromInventory(inventory):
                     print("* Added axe to inventory") # (coins -= 20)
-                elif weaponChoice.isdigit() and int(weaponChoice) == 4 and coins >= 50:
+                    player.addToInventory(axe)
+                    player.loseCoins(20)
+
+                elif weaponChoice.isdigit() and int(weaponChoice) == 4 and coins >= 50 and "sword" not in getItemsFromInventory(inventory):
                     print("* Added sword to inventory") # (coins -= 50)
-                elif weaponChoice.isdigit() and int(weaponChoice) == 5 and coins >= 50:
+                    player.addToInventory(sword)
+                    player.loseCoins(50)
+
+                elif weaponChoice.isdigit() and int(weaponChoice) == 5 and coins >= 50 and "bomb" not in getItemsFromInventory(inventory):
                     print("* Added bomb to inventory") # (coins -= 50)
+                    player.addToInventory(bomb)
+                    player.loseCoins(50)
+
                 elif weaponChoice.isdigit() and int(weaponChoice) == 6:
                     break
                 else:
@@ -77,6 +105,19 @@ def inStore(coins):
                 else:
                     print("Not valid choice")
 
+#Weapons
+knife = Item("knife", 0, 6)
+spear = Item("spear", 1, 8)
+axe = Item("axe", 1, 10)
+sword = Item("sword", 3, 15)
+bomb = Item("bomb", 6, 7)
 
+#Potions
 
-inStore(50)
+# Add to game!
+# Remove items from main
+# Add potions with own Potion class
+
+player = Character(50)
+player.addCoins(100)
+inStore(player)
